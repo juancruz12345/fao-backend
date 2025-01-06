4a65ec308609f79ddf628887da9e7f5b9243266e
juan_cruz <urbanjuancruz@gmail.com>
2025-01-06 15:52:09 -0300

    add

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const db = createClient({ url: process.env.DB_URL, authToken: process.env.DB_TOK
 const ACCEPTED_ORIGINS = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
-  'https://federacionajedrezolavarria.onrender.com/' // Incluye posibles variantes del origen
+  'https://federacionajedrezolavarria.onrender.com' // Incluye posibles variantes del origen
 ];
 
 
